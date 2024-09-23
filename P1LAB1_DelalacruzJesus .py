c12057d6355d9566a1209300fab8e78466df5fb7
delacruj0390 <144360527+delacruj0390@users.noreply.github.com>
2024-09-22 20:33:54 -0400

    Add files via upload

---
+++ P1LAB1_DelalacruzJesus .py
@@ -0,0 +1,11 @@
+# Jesus Delacruz
+# 22 September 2024
+# P1LAB1
+# First Time using IDLE
+
+first_name = input ("Enter your first name: ")
+last_name = input("Enter your last name: ")
+print("Welcome, {firts_name} {last_name} Welcome to CTI-110")
+
+
+
