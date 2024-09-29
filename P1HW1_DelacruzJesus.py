7c840916393ae96d7d479285b2a41d236ddc47e3
delacruj0390 <144360527+delacruj0390@users.noreply.github.com>
2024-09-29 19:22:05 -0400

    Add files via upload

---
+++ P1HW1_DelacruzJesus.py
@@ -0,0 +1,33 @@
+#Jesus Delacruz
+#26September2034
+#P1HW1
+#Code that collects information from user.processes information collected and displays results to user.
+
+
+
+
+
+print("-----Calculating Exponents----")
+#
+#
+
+base_value = int(input("Enter an integer as the base value: "))
+exponent = int(input("Enter an integer as the exponent: "))
+#
+#
+result = base_value ** exponent
+print(f"\n{base_value} raised to the power of {exponent} is {result} !!")
+#
+#
+print("-----Addition and Subtraction----")
+#
+#
+start_value = int(input("Enter a starting integer: "))
+add_value = int(input("Enter an integer to add: "))
+subtract_value = int(input("Enter an integer to subtract: "))
+#
+#
+final_result = start_value + add_value - subtract_value
+print(f"\n{start_value} + {add_value} - {subtract_value} is equal to {final_result}")
+
+
