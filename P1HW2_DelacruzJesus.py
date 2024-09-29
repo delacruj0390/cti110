7f1358180f753b16f2ff86a03f683e32a15c3170
delacruj0390 <144360527+delacruj0390@users.noreply.github.com>
2024-09-29 19:32:32 -0400

    Add files via upload

---
+++ P1HW2_DelacruzJesus.py
@@ -0,0 +1,35 @@
+#TJesus Delacruz
+#September25,2024
+#P1HW2_DelacruzJesus
+#This program calculates travel expenses
+
+
+
+
+
+print('This program calculates and displays travel expenses')
+print()
+budget = int(input('Enter Budget:'))
+print()
+des = input('Enter your travel destination:')
+print()
+gas = int(input('How much do you think you will spend on gas?'))
+print()
+acc = int(input('Approximately, how much will you need for accomodation/hotel?'))
+print()
+food = int(input('Last, how much do you need for food?'))
+
+#Total Values
+exp = food + gas + acc
+remain = budget - exp
+
+print()
+print('----------Travel Expenses----------')
+print('Location: ',des)
+print('Initial Budget:',budget)
+print()
+print('Fuel:',gas)
+print('Accomodation:',acc)
+print('Food: ',food)
+print()
+print('Remaining Balance:',remain)
